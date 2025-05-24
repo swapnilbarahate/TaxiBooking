42836131f637494251d85c6c37be14058b41a2d0
swapnilbarahate <barahateswapnil2017@gmail.com>
2025-05-25 04:36:05 +0530

    set navigation bar color

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -49,6 +49,10 @@
             border-color: #86b7fe;
             box-shadow: 0 0 0 0.25rem rgba(13,110,253,.25);
         }
+
+        .navbar-dark .navbar-nav .nav-link {
+            color: #fff;
+        }
     </style>
 </head>
 <body>
