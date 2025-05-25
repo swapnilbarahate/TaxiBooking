426acff9ce94be6df1bbdac5d0decacc00be00b3
swapnilbarahate <barahateswapnil2017@gmail.com>
2025-05-25 23:02:51 +0530

    screen images added

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,6 +6,8 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>{{ config('app.name', 'Taxi Booking') }}</title>
+    <link rel="icon" href="{{ asset('taxi.png') }}" type="image/png">
+
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
